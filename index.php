218075f94faab683677963b5325523e54540c5ef
jklimes12 <jklimes12@seznam.cz>
2018-09-18 12:14:00 +0200

    +

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 $words = array(
-  'pes' => array('hafan ' , 'psisko'),
-  'kocka' => array('micka ' , 'číča'),
-  'auto' => array('vůz ' , 'kára'),
-  'budova' => array('barák ' , 'dům'),
-  'jídlo' => array('pápů ' , 'papí'),
-  'zadek' => array('zadnice ' , 'sedínka')
+  'pes' => array('hafan, ' , 'psisko'),
+  'kocka' => array('micka, ' , 'číča'),
+  'auto' => array('vůz, ' , 'kára'),
+  'budova' => array('barák, ' , 'dům'),
+  'jídlo' => array('pápů, ' , 'papí'),
+  'zadek' => array('zadnice, ' , 'sedínka')
 );
 foreach ($words as $key => $value) {
   echo $key;
-  ?></br><?php
+  echo " - ";
   foreach ($value as $item) {
     echo $item;
 
